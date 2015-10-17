a5038740ebed542e17d69081df3ca1c8f09dbc63
bloodbrand <moguay@gmail.com>
2015-10-17 22:08:47 +0300

    angular & bootstrap experiments

---
+++ angular/js/app.js
@@ -0,0 +1,12 @@
+var testApp = angular.module('testApp', ['ngMessages', 'ngResource']);
+
+testApp.controller('mainController', function($scope, $log, $filter, $resource){
+    /*
+    $scope.name = 'Radu';
+    $scope.formattedName = $filter('uppercase')($scope.name);
+    $log.info($scope.name);
+    $log.info($scope.formattedName);
+    */
+
+
+});
