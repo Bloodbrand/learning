36acf4b7d2c6ce6bf2cf6d7f2cdbc767c272f072
bloodbrand <moguay@gmail.com>
2015-10-18 20:57:31 +0300

    sync

--- angular/js/app.js
+++ angular/js/app.js
@@ -1,12 +1,30 @@
 var testApp = angular.module('testApp', ['ngMessages', 'ngResource']);
 
-testApp.controller('mainController', function($scope, $log, $filter, $resource){
-    /*
+testApp.controller('mainController',
+['$scope', '$log', '$filter', '$resource', '$timeout', '$interval',
+function($scope, $log, $filter, $resource, $timeout, $interval){
+
+/////////////////////////////////////////
     $scope.name = 'Radu';
-    $scope.formattedName = $filter('uppercase')($scope.name);
+    $scope.ticks = 0;
+/////////////////////////////////////////
     $log.info($scope.name);
     $log.info($scope.formattedName);
-    */
+/////////////////////////////////////////
+    $scope.formattedName = $filter('uppercase')($scope.name);
+/////////////////////////////////////////
+    $timeout(function(){
+        $scope.name += " and All!";
+        $scope.formattedName = $filter('uppercase')($scope.name);
+    }, 3000);
 
+    $interval(function(){
+        $scope.ticks++;
+    }, 100);
+/////////////////////////////////////////
+    $scope.toLowercase = function(str){
+        return $filter('lowercase')(str);
+    };
+/////////////////////////////////////////
 
-});
+}]);
